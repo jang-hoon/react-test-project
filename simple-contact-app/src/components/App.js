291e61e662b50bbc944ca6422a4635ad8c64ebab
hoonjang <hoonjang@ex-em.com>
2018-07-30 17:59:13 +0900

    [simple-contact-app] Contact component created.

--- simple-contact-app/src/components/App.js
+++ simple-contact-app/src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import Contact from './Contact';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello!!</h1>
+                <Contact />
             </div>
         );
     }
